17e5d8c822c4b0766b2164eae8c8d461d9588218
ZhangShenao <15201440436@163.com>
2025-08-18 09:59:40 +0800

    使用LangChain调用通义百炼大模型

--- app/langchain/test_langchain.py
+++ app/langchain/test_langchain.py
@@ -10,22 +10,22 @@ load_dotenv()
 # 构造Prompt
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", "你是一个智能的聊天助手"),
+        ("system", "你是一位{domain}领域的专家，擅长解决用户提出的各种问题"),
         ("user", "{input}"),
     ]
 )
 
 # 创建模型
 llm = ChatOpenAI(
-    model="gpt-4o-mini",
-    api_key=os.getenv("OPENAI_API_KEY"),
-    base_url=os.getenv("OPENAI_BASE_URL"),
-    temperature=0,
+    model="qwen-plus",
+    api_key=os.getenv("DASHSCOPE_API_KEY"),
+    base_url=os.getenv("DASHSCOPE_BASE_URL"),
+    temperature=0.6,
 )
 
 # 构造Chain
 chain = prompt | llm | StrOutputParser()
 
 # 调用Chain,获取结果
-result = chain.invoke({"input": "帮我写一首赞美夏天的诗"})
+result = chain.invoke({"domain": "后端开发", "input": "Java和Go语言的区别是什么？"})
 print(result)
